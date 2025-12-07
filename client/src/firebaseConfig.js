7264a25d0de60baaf73c4c5daa43bb859eef7a8c
Dbh <158257326+Dishabh13@users.noreply.github.com>
2025-12-07 18:58:58 +0530

    Clear Firebase config values for security
    Removed sensitive Firebase configuration values.

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Replace these values from your Firebase Web App
 const firebaseConfig = {
-  apiKey: "AIzaSyCW5-R3CjZ0JOGpSx5Rt6rJqYD4TVFugi0",
-  authDomain: "organdonationprojectdbms.firebaseapp.com",
-  projectId: "organdonationprojectdbms",
-  storageBucket: "organdonationprojectdbms.firebasestorage.app",
-  messagingSenderId: "932123808144",
-  appId: "1:932123808144:web:70c435c166f5f5d15fa315"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
